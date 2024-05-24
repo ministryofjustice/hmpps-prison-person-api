c935ae301f0e0f36d0398e247d03689e9ab12786
brightonsbox <15908737+brightonsbox@users.noreply.github.com>
2024-05-24 15:19:20 +0000

    updating project name after deployment from template repository

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonpersonapi/health/HealthInfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsprisonpersonapi/health/HealthInfoTest.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.hmppstemplatepackagename.health
+package uk.gov.justice.digital.hmpps.hmppsprisonpersonapi.health
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test

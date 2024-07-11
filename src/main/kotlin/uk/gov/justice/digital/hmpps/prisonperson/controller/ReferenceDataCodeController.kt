cc940995f10eb3393f37e6adaf76e57cef4f1e30
David Middleton <david.middleton@digital.justice.gov.uk>
2024-07-11 09:42:10 +0100

    Update formatting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/ReferenceDataCodeController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/ReferenceDataCodeController.kt
@@ -9,7 +9,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.prisonperson.dto.ReferenceDataCodeDto
 import uk.gov.justice.digital.hmpps.prisonperson.service.ReferenceDataCodeService
 import uk.gov.justice.hmpps.kotlin.common.ErrorResponse

f0db8aa5ca4dfe271926421654481e7fe1dbbfd6
Jon Brighton <brightonsbox@hotmail.com>
2024-08-16 10:07:47 +0100

    CDPS-917: Creating GET /prisoners/{prisonNumber}/physical-attributes

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/PhysicalAttributesController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/PhysicalAttributesController.kt
@@ -9,12 +9,14 @@ import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonperson.config.PrisonPersonDataNotFoundException
 import uk.gov.justice.digital.hmpps.prisonperson.dto.request.PhysicalAttributesUpdateRequest
 import uk.gov.justice.digital.hmpps.prisonperson.dto.response.PhysicalAttributesDto
 import uk.gov.justice.digital.hmpps.prisonperson.service.PhysicalAttributesService
@@ -24,6 +26,40 @@ import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 @Tag(name = "Physical Attributes", description = "The height and weight of a prisoner")
 @RequestMapping("/prisoners/{prisonerNumber}/physical-attributes", produces = [MediaType.APPLICATION_JSON_VALUE])
 class PhysicalAttributesController(private val physicalAttributesService: PhysicalAttributesService) {
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  @PreAuthorize("hasRole('ROLE_PRISON_PERSON_API__PRISON_PERSON_DATA__RO')")
+  @Operation(
+    description = "Requires role `ROLE_PRISON_PERSON_API__PRISON_PERSON_DATA__RO`",
+    responses = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Returns Prison Person Data",
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Missing required role. Requires ROLE_PRISON_PERSON_API__PRISON_PERSON_DATA__RO",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "Data not found",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  fun getPhysicalAttributes(
+    @Schema(description = "The prisoner number", example = "A1234AA", required = true)
+    @PathVariable
+    prisonerNumber: String,
+  ) = physicalAttributesService.getPhysicalAttributes(prisonerNumber)
+    ?: throw PrisonPersonDataNotFoundException(prisonerNumber)
 
   @PatchMapping
   @ResponseStatus(HttpStatus.OK)

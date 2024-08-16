bd620f1be0b0a559beeaae8cb821ff514f824bb3
Daniel burnley <dan@danops.uk>
2024-08-16 11:04:31 +0100

    CDPS-907: Update schema for SmokerOrVaper

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/dto/request/HealthUpdateRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/dto/request/HealthUpdateRequest.kt
@@ -3,17 +3,25 @@ package uk.gov.justice.digital.hmpps.prisonperson.dto.request
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
 import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED
 import uk.gov.justice.digital.hmpps.prisonperson.utils.NullishUtils
 
 @Schema(
   description = "Request object for updating a prisoner's health information. Can include one or multiple fields. " +
-          "If an attribute is not defined in the map, it will be set as Undefined.",
+          "If an attribute is provided and set to 'null' it will be updated equal to 'null'. " +
+          "If it is not provided it is not updated",
 )
 @JsonInclude(NON_NULL)
 data class HealthUpdateRequest(
-  @Schema(description = "Map of attributes")
+  @Schema(hidden = true)
   private val attributes: MutableMap<String, Any?> = mutableMapOf(),
 ) : MutableMap<String, Any?> by attributes {
-  @Schema(description = "Smoker or vaper. `ReferenceDataCode`.`id`.", example = "SMOKE_NO")
+  @Schema(
+    description = "Smoker or vaper. `ReferenceDataCode`.`id`.",
+    type = "string",
+    example = "SMOKE_NO",
+    requiredMode = NOT_REQUIRED,
+    nullable = true
+  )
   val smokerOrVaper: Nullish<String> = NullishUtils.getAttribute<String>(attributes, "smokerOrVaper")
 }

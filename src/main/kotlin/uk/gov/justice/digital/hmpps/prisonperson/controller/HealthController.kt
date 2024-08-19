f936c3165ba33747b14129af1e36645295eef866
Dani Burnley <daniel.burnley@digital.justice.gov.uk>
2024-08-19 13:04:23 +0100

    CDPS-907: Add HealthController for editing (#59)
    * CDPS-907: Add HealthController for editing
    
    * CDPS-907: Remove logger
    
    * CDPS-907: Remove unused imports
    
    * CDPS-907: Refactor duplicate code
    
    * CDPS-907: Refactor getAttribute
    
    * CDPS-907: Update schema for SmokerOrVaper
    
    * CDPS-907: Formatting
    
    * CDPS-907: Fix tests
    
    * CDPS-907: Linting
    
    ---------
    
    Co-authored-by: Jon Brighton <brightonsbox@hotmail.com>

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/HealthController.kt
@@ -0,0 +1,78 @@
+package uk.gov.justice.digital.hmpps.prisonperson.controller
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.prisonperson.dto.request.HealthUpdateRequest
+import uk.gov.justice.digital.hmpps.prisonperson.dto.response.HealthDto
+import uk.gov.justice.digital.hmpps.prisonperson.service.HealthService
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+@RestController
+@Tag(name = "Health", description = "The health information for a prisoner")
+@RequestMapping("/prisoners/{prisonerNumber}/health", produces = [MediaType.APPLICATION_JSON_VALUE])
+class HealthController(private val healthService: HealthService) {
+  @PatchMapping
+  @ResponseStatus(HttpStatus.OK)
+  @PreAuthorize("hasRole('ROLE_PRISON_PERSON_API__HEALTH__RW')")
+  @Operation(
+    summary = "Updates the health information for a prisoner",
+    description = "Requires role `ROLE_PRISON_PERSON_API__HEALTH__RW`",
+    responses = [
+      ApiResponse(
+        responseCode = "201",
+        description = "Returns prisoner's health information",
+      ),
+      ApiResponse(
+        responseCode = "400",
+        description = "Bad request",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "401",
+        description = "Unauthorized to access this endpoint",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "403",
+        description = "Missing required role. Requires ROLE_PRISON_PERSON_API__HEALTH__RW",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = ErrorResponse::class),
+          ),
+        ],
+      ),
+    ],
+  )
+  fun createOrUpdateHealth(
+    @Schema(description = "The prisoner number", example = "A1234AA", required = true)
+    @PathVariable
+    prisonerNumber: String,
+    @RequestBody
+    @Valid
+    healthUpdateRequest: HealthUpdateRequest,
+  ): HealthDto = healthService.createOrUpdate(prisonerNumber, healthUpdateRequest)
+}
--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/service/HealthService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/service/HealthService.kt
@@ -2,14 +2,41 @@ package uk.gov.justice.digital.hmpps.prisonperson.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.prisonperson.client.prisonersearch.PrisonerSearchClient
+import uk.gov.justice.digital.hmpps.prisonperson.dto.request.HealthUpdateRequest
 import uk.gov.justice.digital.hmpps.prisonperson.dto.response.HealthDto
+import uk.gov.justice.digital.hmpps.prisonperson.jpa.Health
 import uk.gov.justice.digital.hmpps.prisonperson.jpa.repository.HealthRepository
+import uk.gov.justice.digital.hmpps.prisonperson.jpa.repository.ReferenceDataCodeRepository
+import uk.gov.justice.digital.hmpps.prisonperson.utils.PrisonerNumberUtils
+import uk.gov.justice.digital.hmpps.prisonperson.utils.ReferenceCodeUtils
 import kotlin.jvm.optionals.getOrNull
 
 @Service
 @Transactional(readOnly = true)
 class HealthService(
+  private val prisonerSearchClient: PrisonerSearchClient,
   private val healthRepository: HealthRepository,
+  private val referenceDataCodeRepository: ReferenceDataCodeRepository,
 ) {
   fun getHealth(prisonerNumber: String): HealthDto? = healthRepository.findById(prisonerNumber).getOrNull()?.toDto()
+
+  fun createOrUpdate(
+    prisonerNumber: String,
+    request: HealthUpdateRequest,
+  ): HealthDto {
+    val health = healthRepository.findById(prisonerNumber).orElseGet { newHealthFor(prisonerNumber) }.apply {
+      request.smokerOrVaper.apply(
+        this::smokerOrVaper,
+        fun(smokerOrVaper) = ReferenceCodeUtils.toReferenceDataCode(referenceDataCodeRepository, smokerOrVaper),
+      )
+    }
+
+    return healthRepository.save(health).toDto()
+  }
+
+  private fun newHealthFor(prisonerNumber: String): Health {
+    PrisonerNumberUtils.validatePrisonerNumber(prisonerSearchClient, prisonerNumber)
+    return Health(prisonerNumber)
+  }
 }

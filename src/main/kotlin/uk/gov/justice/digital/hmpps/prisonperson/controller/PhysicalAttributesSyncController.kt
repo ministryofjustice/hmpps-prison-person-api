22420d843b23db942854e8b00a363e378e8d3e21
Jon Brighton <brightonsbox@hotmail.com>
2024-09-20 15:13:01 +0200

    CDPS-965: Tweaks to swagger documentation to reflect reality of what syscon send us (#96)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/PhysicalAttributesSyncController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/controller/PhysicalAttributesSyncController.kt
@@ -36,11 +36,9 @@ class PhysicalAttributesSyncController(private val physicalAttributesSyncService
       "<br/><br/>Requires role `ROLE_PRISON_PERSON_API__PHYSICAL_ATTRIBUTES_SYNC__RW`" +
       "<br/><br/>Edits can be made to physical attributes in NOMIS to both the current booking " +
       "and to old bookings. This sync API can handle both by accepting the `appliesFrom` and " +
-      "`appliesTo` timestamps.  For edits to the current booking, `appliesFrom` should be equal to the " +
-      "point in time that the edit happened, and `appliesTo` should be null.  For edits to historical " +
-      "bookings `appliesFrom` should be equal to the booking start date and and `appliesTo` should be " +
-      "equal to the booking end date. The `createdAt` date should always be the point in time that the " +
-      "user made the edit.",
+      "`appliesTo` timestamps - `appliesFrom` should be equal to the booking start date and and " +
+      "`appliesTo` should be equal to the booking end date. The `createdAt` date should always be " +
+      "the point in time that the user made the edit.",
     responses = [
       ApiResponse(
         responseCode = "201",

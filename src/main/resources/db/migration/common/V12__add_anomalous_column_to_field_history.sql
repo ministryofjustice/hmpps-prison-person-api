d1ea122a677a299d62caca2bc5e1d8d732c901f5
David Middleton <david.middleton@digital.justice.gov.uk>
2024-10-16 16:57:13 +0100

    Address PR comments

--- src/main/resources/db/migration/common/V12__add_anomalous_column_to_field_history.sql
+++ src/main/resources/db/migration/common/V12__add_anomalous_column_to_field_history.sql
@@ -1,8 +1,3 @@
 ALTER TABLE field_history
-    ADD COLUMN anomalous boolean;
-UPDATE field_history
-SET anomalous = FALSE
-WHERE 1 = 1;
-ALTER TABLE field_history
-    ALTER COLUMN anomalous SET NOT NULL;
+    ADD COLUMN anomalous boolean NOT NULL DEFAULT FALSE;
 COMMENT ON COLUMN field_history.anomalous IS 'A flag to indicate that when the data was migrated from NOMIS, the booking it was taken from was historical but did not have an end date';

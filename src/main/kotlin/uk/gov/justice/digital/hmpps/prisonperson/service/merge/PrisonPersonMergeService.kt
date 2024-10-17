09a2a3ea39e12e60e9490f375318a9ee979e9f20
Jon Brighton <brightonsbox@hotmail.com>
2024-10-17 15:33:16 +0100

    CDPS-891: Removing misleading telemetry event

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/service/merge/PrisonPersonMergeService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/service/merge/PrisonPersonMergeService.kt
@@ -1,15 +1,11 @@
 package uk.gov.justice.digital.hmpps.prisonperson.service.merge
-
-import com.microsoft.applicationinsights.TelemetryClient
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import uk.gov.justice.digital.hmpps.prisonperson.config.trackEvent
 
 @Service
 class PrisonPersonMergeService(
   private val physicalAttributesMergeService: PhysicalAttributesMergeService,
-  private val telemetryClient: TelemetryClient,
 ) : PrisonPersonMerge {
 
   @Transactional
@@ -24,14 +20,6 @@ class PrisonPersonMergeService(
     ).forEach {
       it.mergeRecords(prisonerNumberFrom, prisonerNumberTo)
     }
-
-    telemetryClient.trackEvent(
-      "prison-person-api-merge-complete",
-      mapOf(
-        "prisonerNumberFrom" to prisonerNumberFrom,
-        "prisonerNumberTo" to prisonerNumberTo,
-      ),
-    )
   }
 
   private companion object {

fb015f451e04091b93a092147c68c5bb64f19eea
David Middleton <122619525+davidatmoj@users.noreply.github.com>
2024-10-30 09:38:20 +0000

    CDPS-1011: Physical characteristics merge (#128)
    Co-authored-by: Jon Brighton <brightonsbox@hotmail.com>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/prisonperson/service/merge/PhysicalAttributesMergeIntTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/prisonperson/service/merge/PhysicalAttributesMergeIntTest.kt
@@ -17,7 +17,14 @@ import org.springframework.test.context.jdbc.Sql
 import software.amazon.awssdk.services.sns.model.MessageAttributeValue
 import software.amazon.awssdk.services.sns.model.PublishRequest
 import uk.gov.justice.digital.hmpps.prisonperson.enums.EventType.PHYSICAL_ATTRIBUTES_UPDATED
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.BUILD
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.FACE
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.FACIAL_HAIR
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.HAIR
 import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.HEIGHT
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.LEFT_EYE_COLOUR
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.RIGHT_EYE_COLOUR
+import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.SHOE_SIZE
 import uk.gov.justice.digital.hmpps.prisonperson.enums.PrisonPersonField.WEIGHT
 import uk.gov.justice.digital.hmpps.prisonperson.enums.Source.DPS
 import uk.gov.justice.digital.hmpps.prisonperson.integration.IntegrationTestBase
@@ -52,12 +59,19 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // ------------------
     // Before the merge:
     // ------------------
-    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrder(-1, -2, -3, -4, -5)
-    assertThat(fieldHistoryIds(PRISONER_MERGE_FROM)).containsExactlyInAnyOrder(-6, -7, -8, -9, -10)
+    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrderElementsOf(PRISONER1_FIELD_IDS)
+    assertThat(fieldHistoryIds(PRISONER_MERGE_FROM)).containsExactlyInAnyOrderElementsOf(PRISONER2_FIELD_IDS)
     expectFieldMetadata(
       PRISONER_MERGE_TO,
       FieldMetadata(PRISONER_MERGE_TO, HEIGHT, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
       FieldMetadata(PRISONER_MERGE_TO, WEIGHT, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACIAL_HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, BUILD, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, SHOE_SIZE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
     )
 
     publishPrisonerMergedMessage(PRISONER_MERGE_TO, PRISONER_MERGE_FROM)
@@ -67,16 +81,30 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // -----------------
     // After the merge:
     // -----------------
-    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrder(-1, -2, -3, -4, -5, -6, -7, -8, -9, -10)
-    fieldHistoryRepository.findAllById(listOf(-6, -7, -8, -9, -10)).forEach {
+    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrderElementsOf(PRISONER1_FIELD_IDS + PRISONER2_FIELD_IDS)
+    fieldHistoryRepository.findAllById(PRISONER2_FIELD_IDS).forEach {
       assertThat(it.mergedFrom).isEqualTo(PRISONER_MERGE_FROM)
       assertThat(it.mergedAt).isEqualTo(ZonedDateTime.now(clock))
     }
     assertThat(fieldHistoryRepository.getReferenceById(-10).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-105).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-205).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-305).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-405).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-505).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-605).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-705).appliesTo).isEqualTo(ZonedDateTime.now(clock))
     expectFieldMetadata(
       PRISONER_MERGE_TO,
       FieldMetadata(PRISONER_MERGE_TO, HEIGHT, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
       FieldMetadata(PRISONER_MERGE_TO, WEIGHT, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACIAL_HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, BUILD, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, SHOE_SIZE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
     )
 
     assertThat(fieldHistoryIds(PRISONER_MERGE_FROM)).isEmpty()
@@ -93,12 +121,19 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // ------------------
     // Before the merge:
     // ------------------
-    assertThat(fieldHistoryIds(PRISONER1)).containsExactlyInAnyOrder(-1, -2, -3, -4, -5)
-    assertThat(fieldHistoryIds(PRISONER2)).containsExactlyInAnyOrder(-6, -7, -8, -9, -10)
+    assertThat(fieldHistoryIds(PRISONER1)).containsExactlyInAnyOrderElementsOf(PRISONER1_FIELD_IDS)
+    assertThat(fieldHistoryIds(PRISONER2)).containsExactlyInAnyOrderElementsOf(PRISONER2_FIELD_IDS)
     expectFieldMetadata(
       PRISONER2,
       FieldMetadata(PRISONER2, HEIGHT, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
       FieldMetadata(PRISONER2, WEIGHT, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, HAIR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, FACIAL_HAIR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, FACE, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, BUILD, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, SHOE_SIZE, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
     )
 
     publishPrisonerMergedMessage(PRISONER2, PRISONER1)
@@ -108,16 +143,30 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // -----------------
     // After the merge:
     // -----------------
-    assertThat(fieldHistoryIds(PRISONER2)).containsExactlyInAnyOrder(-1, -2, -3, -4, -5, -6, -7, -8, -9, -10)
-    fieldHistoryRepository.findAllById(listOf(-1, -2, -3, -4, -5)).forEach {
+    assertThat(fieldHistoryIds(PRISONER2)).containsExactlyInAnyOrderElementsOf(PRISONER1_FIELD_IDS + PRISONER2_FIELD_IDS)
+    fieldHistoryRepository.findAllById(PRISONER1_FIELD_IDS).forEach {
       assertThat(it.mergedFrom).isEqualTo(PRISONER1)
       assertThat(it.mergedAt).isEqualTo(ZonedDateTime.now(clock))
     }
     assertThat(fieldHistoryRepository.getReferenceById(-10).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-105).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-205).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-305).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-405).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-505).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-605).appliesTo).isEqualTo(ZonedDateTime.now(clock))
+    assertThat(fieldHistoryRepository.getReferenceById(-705).appliesTo).isEqualTo(ZonedDateTime.now(clock))
     expectFieldMetadata(
       PRISONER2,
       FieldMetadata(PRISONER2, HEIGHT, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
       FieldMetadata(PRISONER2, WEIGHT, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, HAIR, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, FACIAL_HAIR, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, FACE, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, BUILD, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER2, SHOE_SIZE, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
     )
 
     assertThat(fieldHistoryIds(PRISONER1)).isEmpty()
@@ -137,7 +186,7 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     prisonerSearch.stubGetPrisoner(NO_PRISON_PERSON_DATA)
     assertThat(fieldHistoryIds(NO_PRISON_PERSON_DATA)).isEmpty()
     assertThat(fieldMetadataRepository.findAllByPrisonerNumber(NO_PRISON_PERSON_DATA)).isEmpty()
-    assertThat(fieldHistoryIds(PRISONER_MERGE_FROM)).containsExactlyInAnyOrder(-6, -7, -8, -9, -10)
+    assertThat(fieldHistoryIds(PRISONER_MERGE_FROM)).containsExactlyInAnyOrderElementsOf(PRISONER2_FIELD_IDS)
 
     publishPrisonerMergedMessage(NO_PRISON_PERSON_DATA, PRISONER_MERGE_FROM)
     awaitAtMost30Secs untilCallTo { prisonPersonQueue.countAllMessagesOnQueue() } matches { it == 0 }
@@ -146,16 +195,30 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // -----------------
     // After the merge:
     // -----------------
-    assertThat(fieldHistoryIds(NO_PRISON_PERSON_DATA)).containsExactlyInAnyOrder(-6, -7, -8, -9, -10)
-    fieldHistoryRepository.findAllById(listOf(-6, -7, -8, -9, -10)).forEach {
+    assertThat(fieldHistoryIds(NO_PRISON_PERSON_DATA)).containsExactlyInAnyOrderElementsOf(PRISONER2_FIELD_IDS)
+    fieldHistoryRepository.findAllById(PRISONER2_FIELD_IDS).forEach {
       assertThat(it.mergedFrom).isEqualTo(PRISONER_MERGE_FROM)
       assertThat(it.mergedAt).isEqualTo(ZonedDateTime.now(clock))
     }
     assertThat(fieldHistoryRepository.getReferenceById(-10).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-105).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-205).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-305).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-405).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-505).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-605).appliesTo).isNull()
+    assertThat(fieldHistoryRepository.getReferenceById(-705).appliesTo).isNull()
     expectFieldMetadata(
       NO_PRISON_PERSON_DATA,
       FieldMetadata(NO_PRISON_PERSON_DATA, HEIGHT, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
       FieldMetadata(NO_PRISON_PERSON_DATA, WEIGHT, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, HAIR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, FACIAL_HAIR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, FACE, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, BUILD, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
+      FieldMetadata(NO_PRISON_PERSON_DATA, SHOE_SIZE, ZonedDateTime.parse("2024-01-03T00:00:00+00:00"), USER1),
     )
 
     assertThat(fieldHistoryIds(PRISONER_MERGE_FROM)).isEmpty()
@@ -173,11 +236,18 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // Before the merge:
     // ------------------
     prisonerSearch.stubGetPrisoner(PRISONER_MERGE_TO)
-    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrder(-1, -2, -3, -4, -5)
+    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrderElementsOf(PRISONER1_FIELD_IDS)
     expectFieldMetadata(
       PRISONER_MERGE_TO,
       FieldMetadata(PRISONER_MERGE_TO, HEIGHT, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
       FieldMetadata(PRISONER_MERGE_TO, WEIGHT, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACIAL_HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, BUILD, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, SHOE_SIZE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
     )
     assertThat(fieldMetadataRepository.findAllByPrisonerNumber(NO_PRISON_PERSON_DATA)).isEmpty()
     assertThat(fieldHistoryIds(NO_PRISON_PERSON_DATA)).isEmpty()
@@ -188,11 +258,18 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     // -----------------
     // After the merge:
     // -----------------
-    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrder(-1, -2, -3, -4, -5)
+    assertThat(fieldHistoryIds(PRISONER_MERGE_TO)).containsExactlyInAnyOrderElementsOf(PRISONER1_FIELD_IDS)
     expectFieldMetadata(
       PRISONER_MERGE_TO,
       FieldMetadata(PRISONER_MERGE_TO, HEIGHT, ZonedDateTime.parse("2024-01-10T00:00:00+00:00"), USER1),
       FieldMetadata(PRISONER_MERGE_TO, WEIGHT, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACIAL_HAIR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, FACE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, BUILD, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, LEFT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, RIGHT_EYE_COLOUR, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
+      FieldMetadata(PRISONER_MERGE_TO, SHOE_SIZE, ZonedDateTime.parse("2024-01-05T00:00:00+00:00"), USER1),
     )
 
     assertThat(fieldHistoryIds(NO_PRISON_PERSON_DATA)).isEmpty()
@@ -218,7 +295,17 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
           url = "http://localhost:8080/prisoners/${PRISONER_MERGE_TO}",
           prisonerNumber = PRISONER_MERGE_TO,
           source = DPS,
-          fields = listOf(HEIGHT, WEIGHT),
+          fields = listOf(
+            HEIGHT,
+            WEIGHT,
+            HAIR,
+            FACIAL_HAIR,
+            FACE,
+            BUILD,
+            LEFT_EYE_COLOUR,
+            RIGHT_EYE_COLOUR,
+            SHOE_SIZE,
+          ),
         ),
         description = PHYSICAL_ATTRIBUTES_UPDATED.domainEventDetails!!.description,
         occurredAt = ZonedDateTime.now(clock),
@@ -243,7 +330,17 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
           "prisonerNumberFrom" to PRISONER_MERGE_FROM,
           "prisonerNumberTo" to PRISONER_MERGE_TO,
           "source" to DPS.name,
-          "fields" to listOf(HEIGHT, WEIGHT).toString(),
+          "fields" to listOf(
+            HEIGHT,
+            WEIGHT,
+            HAIR,
+            FACIAL_HAIR,
+            FACE,
+            BUILD,
+            LEFT_EYE_COLOUR,
+            RIGHT_EYE_COLOUR,
+            SHOE_SIZE,
+          ).toString(),
         ),
       ),
       isNull(),
@@ -302,5 +399,56 @@ class PhysicalAttributesMergeIntTest : IntegrationTestBase() {
     const val PRISONER_MERGE_TO = PRISONER1
     const val PRISONER_MERGE_FROM = PRISONER2
     const val NO_PRISON_PERSON_DATA = "Z1234ZZ"
+
+    val PRISONER1_FIELD_IDS = listOf<Long>(
+      -1,
+      -2,
+      -3,
+      -4,
+      -5,
+      -101,
+      -102,
+      -103,
+      -201,
+      -202,
+      -203,
+      -301,
+      -302,
+      -303,
+      -401,
+      -402,
+      -403,
+      -501,
+      -502,
+      -503,
+      -601,
+      -602,
+      -603,
+      -701,
+      -702,
+      -703,
+    )
+
+    val PRISONER2_FIELD_IDS = listOf<Long>(
+      -6,
+      -7,
+      -8,
+      -9,
+      -10,
+      -104,
+      -105,
+      -204,
+      -205,
+      -304,
+      -305,
+      -404,
+      -405,
+      -504,
+      -505,
+      -604,
+      -605,
+      -704,
+      -705,
+    )
   }
 }

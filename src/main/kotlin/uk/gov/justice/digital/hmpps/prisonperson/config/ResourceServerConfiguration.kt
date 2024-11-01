9f10d64e3ed966a57035ec19bd7f84e9d3f200fe
Jon Brighton <brightonsbox@hotmail.com>
2024-11-01 10:21:46 +0000

    CDPS-1019: Fixing DLQ retry cronjob

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonperson/config/ResourceServerConfiguration.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.prisonperson.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import uk.gov.justice.hmpps.kotlin.auth.dsl.ResourceServerConfigurationCustomizer
+
+@Configuration
+class ResourceServerConfiguration {
+  @Bean
+  fun resourceServerCustomizer() = ResourceServerConfigurationCustomizer {
+    unauthorizedRequestPaths {
+      addPaths = setOf("/queue-admin/retry-all-dlqs")
+    }
+  }
+}

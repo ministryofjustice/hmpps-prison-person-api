a2cda76a65f88fffa0f7955764d536e4e742921f
Dani Burnley <dan@danops.uk>
2024-11-13 15:55:33 +0000

    CDPS-1042: Migration casing

--- src/main/resources/db/migration/common/V17__rename_identifying_marks_to_distinguishing_marks.sql
+++ src/main/resources/db/migration/common/V17__rename_identifying_marks_to_distinguishing_marks.sql
@@ -1,21 +1,21 @@
 ALTER TABLE identifying_mark RENAME TO distinguishing_mark;
 
-alter table distinguishing_mark
-    rename column identifying_mark_id to distinguishing_mark_id;
+ALTER TABLE distinguishing_mark
+    RENAME COLUMN identifying_mark_id TO distinguishing_mark_id;
 
-alter table distinguishing_mark
-    rename constraint identifying_mark_pk to distinguishing_mark_pk;
+ALTER TABLE distinguishing_mark
+    RENAME CONSTRAINT identifying_mark_pk TO distinguishing_mark_pk;
 
 ALTER TABLE identifying_mark_image RENAME TO distinguishing_mark_image;
 
-alter table distinguishing_mark_image
-    rename constraint identifying_mark_image_pk to distinguishing_mark_image_pk;
+ALTER TABLE distinguishing_mark_image
+    RENAME CONSTRAINT identifying_mark_image_pk TO distinguishing_mark_image_pk;
 
-alter table distinguishing_mark_image
-    rename constraint identifying_mark_image_identifying_mark_fk to distinguishing_mark_image_distinguishing_mark_fk;
+ALTER TABLE distinguishing_mark_image
+    RENAME CONSTRAINT identifying_mark_image_identifying_mark_fk TO distinguishing_mark_image_distinguishing_mark_fk;
 
-alter table distinguishing_mark_image
-    rename column identifying_mark_id to distinguishing_mark_id;
+ALTER TABLE distinguishing_mark_image
+    RENAME COLUMN identifying_mark_id TO distinguishing_mark_id;
 
-alter table distinguishing_mark_image
-    rename column identifying_mark_image_id to distinguishing_mark_image_id;
+ALTER TABLE distinguishing_mark_image
+    RENAME COLUMN identifying_mark_image_id TO distinguishing_mark_image_id;
